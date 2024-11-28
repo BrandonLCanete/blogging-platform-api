928b2f9a6decfb1d1e179d6211857cc4e0d618b2
Thirdy Gayares <gayaresthird@gmail.com>
2024-11-29 00:40:36 +0800

    fix: add user role on schema

--- app/schema/user_schema.py
+++ app/schema/user_schema.py
@@ -3,6 +3,7 @@ from marshmallow import Schema, fields
 class UserSchema(Schema):
     user_id = fields.Int(dump_only=True)
     user_uuid = fields.Str(dump_only=True)
+    user_role = fields.Str(required=True)
     username = fields.Str(required=True)
     email = fields.Email(required=True)
     password = fields.Str(required=True, load_only=True)
